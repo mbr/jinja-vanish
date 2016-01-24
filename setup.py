b57afd1a58779bf17e97502d6f5640cf28598641
Marc Brinkmann <git@marcbrinkmann.de>
2016-01-24 22:53:59 +0100

    YaPF and add jinja2 dependency.

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ import os
 
 from setuptools import setup, find_packages
 
+
 def read(fname):
     buf = open(os.path.join(os.path.dirname(__file__), fname), 'rb').read()
     return buf.decode('utf8')
 
-setup(
-    name='jinja-vanish',
-    version='0.1.dev1',
-    description='Customizable auto-escaping for jinja2.',
-    long_description=read('README.rst'),
-    author='Marc Brinkmann',
-    author_email='git@marcbrinkmann.de',
-    url='https://github.com/mbr/jinja-vanish',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    install_requires=[],
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ]
-)
+
+setup(name='jinja-vanish',
+      version='0.1.dev1',
+      description='Customizable auto-escaping for jinja2.',
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='https://github.com/mbr/jinja-vanish',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['jinja2'],
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
